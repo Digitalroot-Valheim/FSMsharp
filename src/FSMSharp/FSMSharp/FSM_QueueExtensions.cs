8ca004ee4e88ccc6ad7bc04712db01f0c2bab3a0
Nicholas Dunnaway <nick@dunnaway.org>
2021-09-03 23:04:33 -0700

    Initial Checking after port

--- src/FSMSharp/FSMSharp/FSM_QueueExtensions.cs
+++ src/FSMSharp/FSMSharp/FSM_QueueExtensions.cs
@@ -1,99 +1,120 @@
-﻿namespace FSMSharp
+﻿using JetBrains.Annotations;
+
+namespace FSMSharp
 {
+  /// <summary>
+  /// Extension methods to easily use the FSM as a queueing of consecutive states.
+  /// </summary>
+  [UsedImplicitly]
+  // ReSharper disable once InconsistentNaming
+  public static class FSM_QueueExtensions
+  {
     /// <summary>
-    /// Extension methods to easily use the FSM as a queueing of consecutive states.
+    /// Queues a new state to specified FSM.
     /// </summary>
-    public static class FSM_QueueExtensions
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<int> Queue(this FSM<int> fsm)
     {
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<int> Queue(this FSM<int> fsm)
-        {
-            return fsm.Add(fsm.Count)
-                .GoesTo(fsm.Count);
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<long> Queue(this FSM<long> fsm)
-        {
-            return fsm.Add(fsm.Count)
-                .GoesTo(fsm.Count);
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<uint> Queue(this FSM<uint> fsm)
-        {
-            return fsm.Add((uint)fsm.Count)
-                .GoesTo((uint)(fsm.Count));
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<ulong> Queue(this FSM<ulong> fsm)
-        {
-            return fsm.Add((ulong)fsm.Count)
-                .GoesTo((ulong)(fsm.Count));
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<short> Queue(this FSM<short> fsm)
-        {
-            return fsm.Add((short)fsm.Count)
-                .GoesTo((short)(fsm.Count));
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<ushort> Queue(this FSM<ushort> fsm)
-        {
-            return fsm.Add((ushort)fsm.Count)
-                .GoesTo((ushort)(fsm.Count));
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<byte> Queue(this FSM<byte> fsm)
-        {
-            return fsm.Add((byte)fsm.Count)
-                .GoesTo((byte)(fsm.Count));
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<sbyte> Queue(this FSM<sbyte> fsm)
-        {
-            return fsm.Add((sbyte)fsm.Count)
-                .GoesTo((sbyte)(fsm.Count + 1));
-        }
-        /// <summary>
-        /// Queues a new state to specified FSM.
-        /// </summary>
-        /// <param name="fsm">The FSM.</param>
-        /// <returns>The newly created state.</returns>
-        public static FsmStateBehaviour<char> Queue(this FSM<char> fsm)
-        {
-            return fsm.Add((char)fsm.Count)
-                .GoesTo((char)(fsm.Count + 1));
-        }
+      return fsm.Add(fsm.Count)
+        .GoesTo(fsm.Count);
     }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<long> Queue(this FSM<long> fsm)
+    {
+      return fsm.Add(fsm.Count)
+        .GoesTo(fsm.Count);
+    }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<uint> Queue(this FSM<uint> fsm)
+    {
+      return fsm.Add((uint)fsm.Count)
+        .GoesTo((uint)(fsm.Count));
+    }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<ulong> Queue(this FSM<ulong> fsm)
+    {
+      return fsm.Add((ulong)fsm.Count)
+        .GoesTo((ulong)(fsm.Count));
+    }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<short> Queue(this FSM<short> fsm)
+    {
+      return fsm.Add((short)fsm.Count)
+        .GoesTo((short)(fsm.Count));
+    }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<ushort> Queue(this FSM<ushort> fsm)
+    {
+      return fsm.Add((ushort)fsm.Count)
+        .GoesTo((ushort)(fsm.Count));
+    }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<byte> Queue(this FSM<byte> fsm)
+    {
+      return fsm.Add((byte)fsm.Count)
+        .GoesTo((byte)(fsm.Count));
+    }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<sbyte> Queue(this FSM<sbyte> fsm)
+    {
+      return fsm.Add((sbyte)fsm.Count)
+        .GoesTo((sbyte)(fsm.Count + 1));
+    }
+
+    /// <summary>
+    /// Queues a new state to specified FSM.
+    /// </summary>
+    /// <param name="fsm">The FSM.</param>
+    /// <returns>The newly created state.</returns>
+    [UsedImplicitly]
+    public static FSM_StateBehaviour<char> Queue(this FSM<char> fsm)
+    {
+      return fsm.Add((char)fsm.Count)
+        .GoesTo((char)(fsm.Count + 1));
+    }
+  }
 }
---
+++ src/FSMSharp/FSMSharp/FSM_Snapshot.cs
@@ -0,0 +1,14 @@
+﻿namespace FSMSharp
+{
+  public readonly struct FSM_Snapshot<T>
+  {
+    internal T CurrentState { get; }
+    internal float StateAge { get; }
+
+    internal FSM_Snapshot(float age, T currentState)
+    {
+      CurrentState = currentState;
+      StateAge = age;
+    }
+  }
+}
